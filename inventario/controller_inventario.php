b94e52f53e38027d1e3af1cad6b643f9a956a226
adrian <adrian.adrianborquez@gmail.com>
2022-11-12 20:08:15 -0700

    Se modifico metodo para registrar articulo en inventario.

--- inventario/controller_inventario.php
+++ inventario/controller_inventario.php
@@ -7,16 +7,18 @@ class ControllerInventario {
             $stockBajo = $data['stock_bajo'];
             $stockOptimo = $data['stock_optimo'];
             $idProveedor = $data['id_proveedor'];
+            $compraDefecto = $data['compra_defecto'];
 
             $db = Conexion::getConexionBd();
 
-            $query = "insert into inventario values (null ,:id_articulo ,:stock ,:stock_bajo, :stock_optimo , :id_proveedor)";
+            $query = "insert into inventario values (null ,:id_articulo ,:stock ,:stock_bajo, :stock_optimo , :id_proveedor , :compra_defecto)";
             $statement = $db->prepare($query);
             $statement->bindParam(":id_articulo",$idArticulo);
             $statement->bindParam(":stock",$stock);
             $statement->bindParam(":stock_bajo",$stockBajo);
             $statement->bindParam(":stock_optimo",$stockOptimo);
             $statement->bindParam(":id_proveedor",$idProveedor);
+            $statement->bindParam(":compra_defecto",$compraDefecto);
 
             $statement->execute();
 
